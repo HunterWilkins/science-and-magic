dff244559226c3582ea20e3bddad00f7b5ea8066
HunterWilkins <wilkins.hunter@gmail.com>
2019-05-15 23:16:35 -0500

    Added / in route

--- server.js
+++ server.js
@@ -10,7 +10,9 @@ app.use(express.urlencoded({extended : true}));
 app.use(express.json());
 
 
-
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
 app.get("/home", function(req, res){
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
