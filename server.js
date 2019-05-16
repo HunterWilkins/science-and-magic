9408f2ea9f3e1f6247cf82cc0240f06fda9581d4
HunterWilkins <wilkins.hunter@gmail.com>
2019-05-15 23:10:51 -0500

    Changed 'home.html' to 'index.html'

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 
 app.get("/home", function(req, res){
-    res.sendFile(path.join(__dirname, "/public/home.html"));
+    res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
 app.get("/artwork", function(req, res){
